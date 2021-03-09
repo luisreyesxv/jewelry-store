77e7be36a0d11d990fd8af159b1284b48d7fb782
Luis Reyes <luisreyesxv@yahoo.com>
2021-03-09 02:54:00 -0500

    changed material buttons and logic. it will display up to 4 materials and then do a 'show more'

--- src/Component/Items/ItemCard.jsx
+++ src/Component/Items/ItemCard.jsx
@@ -12,89 +12,66 @@ const ItemCard=(props)=>{
     const [materialPicked,setMaterialPicked]= useState(props.materials[0].name)
     
 
-    const changeMaterial =(e,object)=>{
+    const changeMaterial =(e)=>{
        e.preventDefault()
-        setMaterialPicked(object.["data-name"])
+        setMaterialPicked(e.target.dataset.name)
     }
 
     const itemURL= `items/${props.slug}?material:${materialPicked}`
 
 
 
-    const metaDescriptions =()=>{
-        return props.materials.map((element)=> (
-            <>
-           {/* <Card.Content key={props.name+" "+element.name}> */}
-                <Button compact disabled={materialPicked=== element.name} key={props.name+" "+element.name} title ={element.name} size="small" icon="square" data-name={element.name} style={{backgroundColor:"transparent",color:materialColors[element.name]}} onClick={changeMaterial}/> 
-                {/* </Card.Content> */}
-          {/* { Math.floor(Math.random() * (2) )?  <Button  disabled={materialPicked=== "silver"} key={props.name+" silver"} title ={element.name} size="small" icon="square" data-name={"silver"} style={{backgroundColor:"transparent",color:materialColors[element.name]}} onClick={changeMaterial}/>
-          : null}  */}
-          
-          {/* {Math.floor(Math.random() * (2) )?   <Button  disabled={materialPicked=== "silver"} key={props.name+"silver"} size="small" icon="circle thin" data-name={"silver"} style={{color:materialColors["silver"]}} onClick={changeMaterial} data-name={"silver"} />
-          : null}
-             */}
-          </>
-        ))
+    const metaDescriptions =()=>{  
+            return props.materials.map((element,index)=> {
+
+                                    if(index===4){
+                                        return (
+                                                <Link to={itemURL} title= "see more styles" >
+                                                        <div className ="itemCardMaterialsButton" style={{border: "none",fontWeight: "bold"}}  key={props.name+" more button"} >
+                                                            See More
+                                                        </div>
+                                                </Link>
+                                        )
+                                    }
+                                    else if (index <4){
+                                        return (
+                                                
+                                                    <div 
+                                                    key={props.name+" "+element.name}
+                                                    title= {element.name}
+                                                    data-name={element.name}
+                                                    className ="itemCardMaterialsButton" 
+                                                    style={
+                                                        {backgroundColor:materialColors[element.name],
+                                                        opacity: materialPicked===element.name? "50%":null}} 
+                                                        onClick={changeMaterial}  
+                                                        />
+                                        )
+                                        
+                                    }
+                            }
+            )
         
     }
 
 
     return(
-        // <Card className="itemCard" title={props.name+" "+ materialPicked} >
-        //         <Image as={Link} to={itemURL} size="medium" src={props.images[0].image_url} />
-        //     {/* <Card.Header> */}
-        //     <Grid centered>
-        //     <Grid.Row  >
-            
-        //     {metaDescriptions()}
-            
-        // </Grid.Row>
-       
-        //             <Grid.Column width="15" floated="left">
-        //                 <Card.Content className="itemCardHeader" as={Link} to={itemURL}>{props.name}</Card.Content>
-        //             </Grid.Column>
-        //             <Grid.Column width="15" floated="right">
-        //                 <Card.Content textAlign="right" verticalalign="top" className="itemCardPrice"> {"$"+parseFloat(props.price).toFixed(2)} </Card.Content>
-        //             </Grid.Column>
-
-                
-        //     </Grid>
-            
-            
-            
-            
-
-        // </Card>
-
-
         <div className="itemCard" >
-            {/* <ul className="itemCardUi" > */}
             <div className="itemCardImageContainer" >
                 <Link to={itemURL} >
-                    <SmoothLoadingImage
-
-                
-                    className="itemImage" 
-                    src={props.images[0].image_url}
-                    
+                    <SmoothLoadingImage     
+                        className="itemImage" 
+                        src={props.images[0].image_url}
                     />
-
                 </Link>
             </div>
-            
-                    <Link className="itemCardHeader"  to={itemURL}>
-                        {/* {props.name.length>20? props.name.slice(0,16)+"...": props.name} */}
-                        {props.name}
-                        </Link>
-                    <div className="itemCardPrice">{"$"+parseFloat(props.price).toFixed(2)}</div>
-                    <div className="itemCardMaterials"  >
-            
-                 {metaDescriptions()}
-                
-             </div>
-            {/* </div> */}
-                {/* luis
-            </ul> */}
+            <Link className="itemCardHeader"  to={itemURL}>
+                {props.name}
+            </Link>
+            <div className="itemCardPrice">{"$"+parseFloat(props.price).toFixed(2)}</div>
+            <div className="itemCardMaterials"  >         
+                    {metaDescriptions()}               
+            </div>
         </div>
     )
 }
