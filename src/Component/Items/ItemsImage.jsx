848e6c857af649b9a415818f497e65e753c8677d
Luis Reyes <luisreyesxv@yahoo.com>
2021-03-15 07:06:46 -0400

    created itemsimage component which current spits out the thumbnails or does placeholder until it loads

---
+++ src/Component/Items/ItemsImage.jsx
@@ -0,0 +1,117 @@
+import React,{useState} from 'react'
+
+import {Grid,Placeholder, List} from 'semantic-ui-react'
+
+import SmoothLoadingImage from '../SmoothLoadingImage'
+
+
+
+
+const ItemsImage = (props)=>{
+    const [imageIndex,setImageIndex] = useState(0)
+
+    const thumbnails = ()=>{
+        return props.images.map(((image,index)=>{
+
+            return (
+                
+                <List.Item active={imageIndex===index} onClick={()=> setImageIndex(index)} >
+                    <SmoothLoadingImage key={"thumbnail" + index} src={image} alt={"item thumbnail #"+index} className="items-thumbnail-image"/>
+                </List.Item>
+               
+                
+            )
+
+        }))
+    }
+    const placeholderThumbnails =()=>{
+        return(
+            <>
+                    <List.Item>
+                        <Placeholder >
+                            <Placeholder.Image className="items-thumbnail-image"/>
+                        </Placeholder>
+                    </List.Item>
+                    <List.Item>
+                        <Placeholder >
+                            <Placeholder.Image className="items-thumbnail-image"/>
+                        </Placeholder>
+                    </List.Item>
+            </>
+
+        )
+    }
+
+
+
+    const imagesFound =()=>{
+        return(
+            <Grid stackable={false} columns="2" divided>
+                <Grid.Column width="4" >
+                    <List selection>
+                        {thumbnails()}
+                    </List>
+                </Grid.Column>
+                <Grid.Column width="10"> 
+                    <SmoothLoadingImage key="main" src={props.images[imageIndex]} id="items-page-main"/>
+                </Grid.Column>
+                
+            </Grid>
+        )
+    }
+
+    const placeholderImage =()=>{
+        return(
+            <Placeholder>
+                            <Placeholder.Image id="items-page-main"/>
+            </Placeholder>
+        )
+    }
+
+    const placeholders =()=>{
+        return(
+            
+                <Grid stackable={false} columns="2" divided>
+                    <Grid.Column width="4" >
+                        <List selection>
+                            <List.Item>
+                            <Placeholder >
+                                <Placeholder.Image className="items-thumbnail-image"/>
+                            </Placeholder>
+                            </List.Item>
+
+                        </List>
+                    </Grid.Column>
+                    <Grid.Column width="10"> 
+                        <Placeholder>
+                            <Placeholder.Image id="items-page-main"/>
+                        </Placeholder>
+                    
+                    </Grid.Column>
+                    
+                </Grid>
+            
+        )
+    }
+
+
+
+    return(
+
+        // props.images? imagesFound() : placeholders()
+        // placeholders()
+        <Grid stackable={false} columns="2" divided>
+                <Grid.Column width="4" >
+                    <List selection={props.images}>
+                        {props.images? thumbnails(): placeholderThumbnails()}
+                    </List>
+                </Grid.Column>
+                <Grid.Column width="10"> 
+                    {props.images? <SmoothLoadingImage key="main" src={props.images[imageIndex]} id="items-page-main"/> : placeholderImage()}
+                </Grid.Column>
+                
+            </Grid>
+    )
+}
+
+export default ItemsImage
--- src/Container/ItemsContainer.jsx
+++ src/Container/ItemsContainer.jsx
@@ -4,6 +4,11 @@ import UserContextConsumer from '../Context/UserContextConsumer'
 import {Route,Switch,useParams, useLocation} from 'react-router-dom'
 import {Grid,Container} from 'semantic-ui-react'
 
+import Normal404Page from '../Component/404/Normal404Page'
+import ItemsImage from '../Component/Items/ItemsImage'
+
+
+
 
 
 const ItemsContainer =(props)=>{
@@ -11,8 +16,8 @@ const ItemsContainer =(props)=>{
   const {slug} = useParams()
   let urlQueryValues = new URLSearchParams(useLocation().search)
   const [item,setItem] = useState()
-  // const [material,setMaterial]= useState(urlQueryValues.get("material"))
   const [material,setMaterial]= useState(urlQueryValues.get("material"))
+  const [itemNotFound,setItemNotFound] = useState(false)
 
 
 
@@ -38,21 +43,23 @@ const ItemsContainer =(props)=>{
             },
             credentials: 'include',
           }
-    
-    
+  
           fetch(process.env.REACT_APP_API_URL+"items/"+slug,options)
-          .then(response=> response.json())
-          .then(itemObject =>{
-            console.log(itemObject)
-            setItem(itemObject)
-    
+          .then(response=> {
+            if(!response.ok){
+             throw new Error()
+            }
+            else return response.json()
+            
           })
+          .then(itemObject =>{
+            setItem(itemObject)
+          })
+          .catch(()=> setItemNotFound(true))
     }
 
 
     const currentMaterial = () =>{
-
-       
       if(item){
         if(!item.images[material]){
           setMaterial( Object.keys(item.images)[0])
@@ -60,29 +67,35 @@ const ItemsContainer =(props)=>{
       }
     }
 
-    return (
-      <Container>
-        <Grid  verticalAlign="middle" className="page-container-grid" >
-          <Grid.Row>
-              <h1>{item?.name}</h1>
-                <p> {props.user}</p>
-          </Grid.Row>
-          <Grid.Row columns="2">
-              <Grid.Column>
-                  <img src={item?.images[material]?.[0]} />
-              </Grid.Column>
-              <Grid.Column>
-                {slug}
-                <p>{item?.description}</p>
-                {"luis"+material}
-              </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
+
+    const fileExists = ()=>{
+
+            return (
+              <Container>
+                <Grid  verticalAlign="middle" className="page-container-grid" >
+                  <Grid.Row centered="true">
+                      <h1>{item?.name}</h1>
+                        <p> {props.user}</p>
+                  </Grid.Row>
+                  <Grid.Row columns="2" >
+                      <Grid.Column mobile="16" computer ="8" tablet="8">
+                          {/* <img src={item?.images[material]?.[0]} /> */}
+                          <ItemsImage  images={item?.images[material]} />
+                      </Grid.Column>
+                      <Grid.Column mobile="16" computer ="8" tablet="8">
+                        {slug}
+                        <p>{item?.description + item?.description }</p>
+                        {"luis"+material}
+                      </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </Container>
+            )
+    }
 
 
-
-
+    return(
+      itemNotFound?  <Normal404Page />: fileExists()
     )
 }
 
