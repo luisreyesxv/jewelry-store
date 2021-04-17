c9ea71470514afe60eb78d7ac1a55c32f9604cc7
Luis Reyes <luisreyesxv@yahoo.com>
2021-04-17 18:38:11 -0400

    broken checkout page into accordion. started set up

---
+++ src/Component/Checkout/Cart.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import {Grid, Button,Header, Icon} from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
+
+import ShoppingCartItem from '../ShoppingCart/ShoppingCartItem'
+
+
+const CartComponents = (props)=>{
+    const mappingCartItems = ()=>{
+        return props.cart.map(item =>{
+            return(
+                <Grid.Row>
+                    < ShoppingCartItem key={`checkout ${item.slug} - ${item.material}`}  {...item} item={item} changeCart={props.changeCart} disabled={props.cartFinalized}/>
+                </Grid.Row>
+                    
+            )
+        })
+    }
+
+    const emptyCart =()=>{
+
+        return(
+            <Grid>
+                <Grid.Row centered={true}>
+                                
+                    
+                        <Header as={Grid.Row} icon className="checkout-cart-header" >
+                            <Icon name="cart" size="massive"  className="checkout-cart-header" />
+                            <h1 className="checkout-cart-header"> Cart's Empty</h1>
+                            <Header.Subheader className="checkout-cart-header">
+                                <Button as={Link} to="/"  id="shopping-cart-button">
+                                    See What's For Sale
+                                </Button>
+                            </Header.Subheader>
+                        </Header>
+                    
+
+                </Grid.Row>
+            </Grid>
+        )
+    }
+
+    const existingCart = () =>{
+
+        return(
+        <>
+            <Grid container celled="internally">
+                {mappingCartItems()}
+            </Grid>
+            <Grid.Row>
+                <Button.Group fluid>
+                    <Button id="shopping-cart-button"   onClick={()=>props.setCartFinalized(!props.cartFinalized)}>
+                        {props.cartFinalized? "Edit Cart" : "Finalize Cart"}
+                    </Button>
+                    <Button.Or />
+                    <Button id="shopping-cart-button" successful  onClick={()=>props.setActiveAccordion("shipping")} disabled={!props.cartFinalized}>
+                        Confirm
+                    </Button>
+                </Button.Group>
+            </Grid.Row>
+        </>
+        )
+    }
+
+
+    return(
+       props.cart?.length? existingCart() : emptyCart()
+
+    )
+}
+
+
+
+
+export default CartComponents
--- src/Container/Checkout.jsx
+++ src/Container/Checkout.jsx
@@ -1,10 +1,10 @@
 import React,{useState,useEffect,useCallback} from 'react'
-import {Grid,Container, Button} from 'semantic-ui-react'
+import {Grid,Container, Button, Accordion, Icon, Card} from 'semantic-ui-react'
 
 import CartContextConsumer from '../Context/CartContextConsumer'
 import debounce from 'lodash.debounce'
 
-import ShoppingCartItem from '../Component/ShoppingCart/ShoppingCartItem'
+import CartComponents from '../Component/Checkout/Cart'
 import OrderSummary from '../Component/Checkout/OrderSummary'
 
 
@@ -15,11 +15,12 @@ const CheckoutContainer = (props)=>{
 
     useEffect(()=>{
         document.title= "Checkout -"+ process.env.REACT_APP_TITLE
-        console.log("this ran")
-                    checkout()
-            },[])
+        checkout() },[])
 
-    const [cartFinalized, setCartFinalized] = useState(true)       
+    const [cartFinalized, setCartFinalized] = useState(true)   
+    const [activeAccordion,setActiveAccordion] = useState("cart") 
+    const [shipping, setShipping] = useState({street:"",city: "", state: "", country: "", zip: ""})   
+    const [billing, setBilling] = useState({street:"",city: "", state: "", country: "", zip: ""})   
 
     const checkout=()=>{
         const body ={orders: {items: props.cart}}
@@ -67,17 +68,6 @@ const CheckoutContainer = (props)=>{
     }
 
 
-    const mappingCartItems = ()=>{
-        return props.cart.map(item =>{
-            return(
-                <Grid.Row>
-                    < ShoppingCartItem key={`checkout ${item.slug} - ${item.material}`}  {...item} item={item} changeCart={props.changeCart} disabled={cartFinalized}/>
-                </Grid.Row>
-                    
-            )
-        })
-    }
-
 
     return(
         
@@ -88,19 +78,51 @@ const CheckoutContainer = (props)=>{
                 </Grid.Row>
                 <Grid.Row columns="4" >
                     <Grid.Column width="9">
-                        <Grid container>
-                                {mappingCartItems()}
-                        </Grid>
+                        <Accordion fluid styled className="checkout-accordion"> 
+                            <Accordion.Title active={activeAccordion==="cart"}  onClick={()=>activeAccordion==="cart"?setActiveAccordion("none"):setActiveAccordion("cart")} >
+                               <h2>
+                                   <Icon name="dropdown"/>
+                                   Cart
+                                </h2>
+                            </Accordion.Title>
+                            <Accordion.Content active={activeAccordion==="cart"}>
+                                <CartComponents cart={props.cart}   cartFinalized={cartFinalized} setCartFinalized={setCartFinalized} changeCart={props.changeCart} setActiveAccordion={setActiveAccordion}/>
+                            </Accordion.Content>
+                            <Accordion.Title active={activeAccordion==="shipping"}  onClick={()=>activeAccordion==="shipping"?setActiveAccordion("none"):setActiveAccordion("shipping")} >
+                                <h2>
+                                    <Icon name="dropdown"/>
+                                    Shipping
+                                </h2>
+                            </Accordion.Title>
+                            <Accordion.Content active={activeAccordion==="shipping"}>
+                                Shipping Form
+                            </Accordion.Content>
+                            <Accordion.Title active={activeAccordion==="billing"}  onClick={()=>activeAccordion==="billing"?setActiveAccordion("none"):setActiveAccordion("billing")} >
+                                <h2>
+                                    <Icon name="dropdown"/>
+                                    Billing
+                                </h2>
+                            </Accordion.Title>
+                            <Accordion.Content active={activeAccordion==="billing"}>
+                                Billing Form
+                            </Accordion.Content>
+                            <Accordion.Title active={activeAccordion==="CC"}  onClick={()=>activeAccordion==="CC"?setActiveAccordion("none"):setActiveAccordion("CC")} >
+                                <h2>
+                                    <Icon name="dropdown"/>
+                                    Credit Card Information
+                                </h2>
+                            </Accordion.Title>
+                            <Accordion.Content active={activeAccordion==="CC"}>
+                                Stripe Card Form
+                            </Accordion.Content>
+                            
+                    </Accordion>
                     </Grid.Column>
                     <Grid.Column width="6"  style={{background:"green"}}>
                         <OrderSummary cart={props.cart} />
                     </Grid.Column>
                 </Grid.Row>
-                <Grid.Row>
-                    <Button id="shopping-cart-button" fluid={true}  onClick={()=>setCartFinalized(!cartFinalized)}>
-                         {cartFinalized? "Edit Cart" : "Finalize Cart"}
-                    </Button>
-                </Grid.Row>
+                
             </Grid>
         </Container>
 
